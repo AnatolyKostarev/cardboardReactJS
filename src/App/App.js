9719af2c0fc1d04b9e2a255464e437a3d745af82
AnatolyKostarev <anatolykostarev@gmail.com>
2021-07-06 12:35:26 +0300

    App component - добавлены блоки header, mainblock, footer

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+/* jshint ignore:start*/
+import './App.css';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
+import Mainblock from '../components/mainblock/Mainblock';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Mainblock />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+/* jshint ignore:end*/
