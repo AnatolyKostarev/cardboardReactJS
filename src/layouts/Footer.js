b0df4a013a9e2cd9e3857f88362dbc2434621477
AnatolyKostarev <anatolykostarev@gmail.com>
2021-07-06 12:37:25 +0300

    Added component footer и стили

---
+++ src/layouts/Footer.js
@@ -0,0 +1,17 @@
+/* jshint ignore:start*/
+
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+    <section id="footer">
+        <p class="footer-text">&copy; www.gofra-ural.ru 2021 , все права защищены</p>
+    </section> 
+</footer>
+        </div>
+    )
+}
+/* jshint ignore:end*/
