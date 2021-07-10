961c00e0b768b4935734579aec17e803706dba6f
AnatolyKostarev <anatolykostarev@gmail.com>
2021-07-10 16:14:15 +0300

    Add to index.js. routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 /* jshint ignore:start*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
