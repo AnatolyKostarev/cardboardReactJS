2c3728f95ef40132a1d2ff4e8988c8addbb40f12
AnatolyKostarev <anatolykostarev@gmail.com>
2021-08-07 16:40:58 +0300

    Delete react-reveal animation property

--- src/components/mainblock/Features.js
+++ src/components/mainblock/Features.js
@@ -1,7 +1,6 @@
 /* jshint ignore:start*/
 
 import React from 'react';
-import Fade from 'react-reveal/Fade';
 import '../css/mainblock/Features.css';
 
 export default function Features(props) {
@@ -12,7 +11,7 @@ export default function Features(props) {
                 <div className="features-numbers">
                     {
                         Object.keys(f).map((elem, index) => (
-                            <div className="features-numbers__block" key={index}>
+                            <div className="features-numbers__block " key={index}>
                                 <h2 className="features-number__topic">{f[elem].topic}</h2>
                                 <p className="features-number__description">{f[elem].description}</p>
                             </div>
@@ -22,28 +21,28 @@ export default function Features(props) {
                 <div className="features-cards">
                     {
                         Object.keys(card).map((elem, index) => (
-                            <Fade top delay={card[elem].delay} key={index}>
-                                <div className="features-card__slip" key={index}>
+                        
+                                <div className="features-card__slip animate__animated animate__fadeInDown animate__delay" key={index}>
                                     <div className="features-img__left">
                                         <img src={card[elem].pic} alt={card[elem].title} />
                                     </div>
                                     <h4 className="features-item" >{card[elem].title}</h4>
                                     <p className="features-text" >{card[elem].subtitle}</p> 
                                 </div>
-                            </Fade>
+                        
                         ))
                     }
                     {
                         Object.keys(slip).map((elem, index) => (
-                            <Fade top delay ={slip[elem].delay} key={index}>
-                                <div className="features-card__slip card__slip" key={index}>
+                            // <Fade top delay ={slip[elem].delay} key={index}>
+                                <div className="features-card__slip card__slip animate__animated animate__fadeInDown" data-wow-delay="2s" key={index}>
                                     <div className="features-img__left">
                                         <img src={slip[elem].pic} alt={slip[elem].title} />
                                     </div>
                                     <h4 className="features-item" >{slip[elem].title}</h4>
                                     <p className="features-text" >{slip[elem].subtitle}</p> 
                                 </div>
-                            </Fade>
+                            // </Fade>
                         ))
                     }
                 </div>
